84148f330dcfbcf3a9f051d6eb9ae423632843d5
DESKTOP-8R3FD9M\hooloop_PC03 <thomaskao@hooloop.com>
2019-04-03 10:47:05 +0800

    quadtree init

---
+++ Assets/MapTile/README.md
@@ -0,0 +1,3 @@
+# MapTile with Quadtrees
+[Quadtrees](https://www.i-programmer.info/programming/theory/1679-quadtrees-and-octrees.html)
+[A Simple QuadTree Implementation in C#](https://www.codeproject.com/Articles/30535/A-Simple-QuadTree-Implementation-in-C)
---
+++ Assets/MapTile/Scripts/TileLayouter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileLayouter : MonoBehaviour
+{
+    public float squareLength = 640;
+    public int minZoom = 12;
+    public int maxZoom = 18;
+
+    public SquareTile squareTilePrefab;
+
+    public void LayoutTiles()
+    {
+        // minZoom      -> 2^0
+        // mizZoom+1    -> 2^2
+        // mizZoom+2    -> 2^4
+
+    }
+}
